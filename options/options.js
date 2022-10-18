e7b31436823a4d6c149ef6c79dd7c30fb46aecf8
Aline Tavares <aline@crowdbotics.com>
2022-10-18 11:58:36 -0400

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "app_generation_1018_39299",
   url: "https://app_generation_1018_39299.botics.co",
   api: "https://app_generation_1018_39299.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.FNefbHRO}>
+      <View style={_styles.otElLBdw}></View></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  FNefbHRO: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  otElLBdw: {
+    left: 71,
+    top: 99,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
